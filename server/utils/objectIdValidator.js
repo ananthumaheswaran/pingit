3988cb36a84961df0df47aa64ffbdf782f7554c1
Ananthu Maheswaran <ananthumaheswaran098@gmail.com>
2026-08-08 00:04:28 +0530

    feat(util): add reusable ObjectId validator

--- server/utils/objectIdValidator.js
+++ server/utils/objectIdValidator.js
@@ -1,19 +1,9 @@
 import mongoose from "mongoose";
 
+/**
+ * Check whether a value is a valid MongoDB ObjectId.
+ *
+ * @param {string} id - Value to validate.
+ * @returns {boolean}
+ */
 export const isValidObjectId = (id) => mongoose.isValidObjectId(id);
-
-// /**
-//  * Validates whether a value is a valid MongoDB ObjectId.
-//  *
-//  * @param {string} id - The ID to validate.
-//  * @param {Function} next - Express next() function.
-//  * @param {string} [message="Invalid Object ID"] - Custom error message.
-//  * @returns {boolean} - Returns true if valid, false if invalid.
-//  */
-// export const validateObjectId = (id, next, message = "Invalid Object ID") => {
-//   if (!mongoose.isValidObjectId(id)) {
-//     next(new AppError(message, 400));
-//     return false;
-//   }
-//   return true;
-// };
