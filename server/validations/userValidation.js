ea086b91b3c1c4dece75c98909f140d299fedb38
Ananthu Maheswaran <ananthumaheswaran098@gmail.com>
2026-08-10 21:40:12 +0530

    feat(validation): add Joi schemas for users

--- server/validations/userValidation.js
+++ server/validations/userValidation.js
@@ -1,50 +1,90 @@
 import Joi from "joi";
-import { username, email, password, name, bio } from "./commonValidation";
+import {
+  username,
+  email,
+  password,
+  name,
+  bio,
+  emailOrUsername,
+  searchQuery,
+  objectId,
+  cloudinaryImage,
+} from "./commonValidation.js";
 
 //Registration validation schema
-
-export const registerSchema = Joi.object({
-  username: username.required(),
-  email: email.required(),
-  password: password.required(),
-  name: name.required(),
-});
-
-// Login validation schema
-export const loginSchema = Joi.object({
-  emailOrUsername: Joi.string().required().messages({
-    "string.empty": "Email or username is required",
+export const registerSchema = {
+  body: Joi.object({
+    username: username.required(),
+    email: email.required(),
+    password: password.required(),
+    name: name.required(),
   }),
-  password: password.required(),
-});
+};
+// Login validation schema
+export const loginSchema = {
+  body: Joi.object({
+    emailOrUsername: emailOrUsername.required(),
+    password: password.required(),
+  }),
+};
 
 // Change password shema
-export const changePasswordSchema = Joi.object({
-  currentPassword: password.required().label("Current Password"),
-  newPassword: password.required().label("New Password"),
-  confirmNewPassword: Joi.string()
-    .required()
-    .valid(Joi.ref("newPassword"))
-    .messages({
-      "any.only": "Confirm password must match new password",
-      "string.empty": "Confirm password is required",
-    }),
-});
+export const changePasswordSchema = {
+  body: Joi.object({
+    currentPassword: password.required().label("Current Password"),
+    newPassword: password.required().label("New Password"),
+    confirmNewPassword: Joi.string()
+      .required()
+      .valid(Joi.ref("newPassword"))
+      .messages({
+        "any.only": "Confirm password must match new password",
+        "string.empty": "Confirm password is required",
+      }),
+  }),
+};
 
 // Change email schema
-export const changeEmailSchema = Joi.object({
-  currentPassword: password.required().label("Current Password"),
-  newEmail: email.required().label("New Email"),
-});
+export const changeEmailSchema = {
+  body: Joi.object({
+    currentPassword: password.required().label("Current Password"),
+    newEmail: email.required().label("New Email"),
+  }),
+};
 
 // Confirm password schema for account deletion
-export const confirmPasswordSchema = Joi.object({
-  currentPassword: password.required().label("Current Password"),
-});
+export const confirmPasswordSchema = {
+  body: Joi.object({
+    currentPassword: password.required().label("Current Password"),
+  }),
+};
 
-// Update profile schema
-export const updateProfileSchema = Joi.object({
-  username: username.optional(),
-  name: name.optional(),
-  bio: bio.optional(),
-});
+// Update user profile schema
+export const updateUserProfileSchema = {
+  body: Joi.object({
+    username: username.optional(),
+    name: name.optional(),
+    bio: bio.optional(),
+    image: cloudinaryImage.optional(),
+  }),
+};
+
+// Search user schema
+export const searchUsersSchema = {
+  query: Joi.object({
+    search: searchQuery.required(),
+  }),
+};
+
+export const userIdSchema = {
+  params: Joi.object({
+    userId: objectId.required(),
+  }),
+};
+
+// Follow list schema
+export const getFollowListSchema = {
+  params: Joi.object({
+    userId: objectId.required(),
+    type: Joi.string().valid("followers", "following").required(),
+  }),
+};
