bf13f1239c3c9d5b4bcb6730960bd87e7362ade4
Ananthu Maheswaran <ananthumaheswaran098@gmail.com>
2026-08-10 17:26:10 +0530

    feat: add common Joi validation schemas

--- server/validations/commonValidation.js
+++ server/validations/commonValidation.js
@@ -1,7 +1,9 @@
 import Joi from "joi";
+import mongoose from "mongoose";
 
 export const username = Joi.string()
   .trim()
+  .lowercase()
   .min(3)
   .max(25)
   .pattern(/^(?!.*\.\.)(?!.*\.$)[a-zA-Z0-9._]+$/)
@@ -15,6 +17,7 @@ export const username = Joi.string()
 
 export const email = Joi.string()
   .trim()
+  .lowercase()
   .email({ tlds: { allow: false } })
   .messages({
     "string.empty": "Email is required",
@@ -25,8 +28,8 @@ export const password = Joi.string()
   .trim()
   .pattern(
     new RegExp(
-      "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,64}$"
-    )
+      "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,64}$",
+    ),
   )
   .messages({
     "string.empty": "Password is required",
@@ -53,19 +56,45 @@ export const bio = Joi.string()
     "string.pattern.base": "Bio contains invalid characters",
   });
 
-export const contentSchema = Joi.string()
+export const content = Joi.string()
   .trim()
   .min(1)
   .max(500)
   .pattern(/^(?!.*<[^>]*>)[\s\S]*$/, "no HTML tags") // prevent HTML
   .messages({
-    "string.empty": "Post content is required",
-    "string.min": "Post content must contain at least 1 character",
-    "string.max": "Post content must be less than or equal to 500 characters",
-    "string.pattern.name": "Post content must not contain HTML tags",
+    "string.empty": "Content is required",
+    "string.min": "Content must contain at least 1 character",
+    "string.max": "Content must be less than or equal to 500 characters",
+    "string.pattern.name": "Content must not contain HTML tags",
   });
 
-export const objectId = Joi.string().length(24).hex().messages({
-  "string.length": "Invalid ID format",
-  "string.hex": "ID must be a valid hex string",
+export const objectId = Joi.string()
+  .custom((value, helpers) => {
+    if (!mongoose.isValidObjectId(value)) {
+      return helpers.error("any.invalid");
+    }
+
+    return value;
+  })
+  .messages({
+    "any.invalid": "Invalid ID format",
+  });
+
+export const emailOrUsername = Joi.string()
+  .trim()
+  .lowercase()
+  .messages({ "string.empty": "Email or username is required" });
+
+export const searchQuery = Joi.string()
+  .trim()
+  .max(25)
+  .pattern(/^[a-zA-Z0-9._]+$/)
+  .messages({
+    "string.empty": "Search query is required",
+    "string.max": "Search query cannot exceed 25 characters",
+  });
+
+export const cloudinaryImage = Joi.object({
+  url: Joi.string().uri().required(),
+  publicId: Joi.string().required(),
 });
